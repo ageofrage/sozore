3f996062dce12c44e628f1acf5162d5cd04f9851
ageofrage <agehara.dev@gmail.com>
2024-06-15 15:42:10 +0900

    fix config

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import {resolve} from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/main.ts'),
+      formats: ['es', 'cjs'],
+      fileName: (format) => `sozore.${format}.js`
+    }
+  },
+  rollupOptions: {
+    output: {
+      preserveModules: true,
+      exports: 'named',
+    }
+  },
+})
